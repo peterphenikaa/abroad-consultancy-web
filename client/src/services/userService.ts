1280a0b1ecd23df19526d4b282373a1643d66e93
dnhfan <dinhan.0321@gmail.com>
2026-05-30 01:56:18 +0700

    feat(user-service): add userSerivce in client to send req

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -2,13 +2,18 @@ import type { MeResponse, UpdateProfileData } from "@/types/user";
 import apiClient from "./apiClient";
 
 export const getMyProfile = async (): Promise<MeResponse> => {
-  const res = await apiClient.get<MeResponse>("/users/me");
-  return res.data;
+  const res = await apiClient.get<{ success: boolean; data: MeResponse }>(
+    "/users/me",
+  );
+  return res.data.data;
 };
 
 export const updateMyProfile = async (
   data: UpdateProfileData,
 ): Promise<MeResponse> => {
-  const res = await apiClient.put("/users/me", data);
-  return res.data;
+  const res = await apiClient.put<{ success: boolean; data: MeResponse }>(
+    "/users/me",
+    data,
+  );
+  return res.data.data;
 };
